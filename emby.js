c72ec4e0ce215e4e255f1a5cedd0c2dbfdc11d76
Ma <sjtumaxuewen@gmail.com>
2023-04-29 20:27:49 +0800

    Create emby.js

---
+++ emby.js
@@ -0,0 +1,9 @@
+(function (){
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://danmaku.movie.kg/ext.js';
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://danmaku.movie.kg/ext.css';
+    document.body.appendChild(script).appendChild(link);
+}())
